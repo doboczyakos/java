7f5a253c8874909151bfd5cd8addb5489f02b8f6
Dobóczy Ákos <akos.doboczy@tel.io>
2024-11-24 22:25:39 +0100

    Regisztráció

--- Gyakorlat/src/main/java/com/example/gyakorlat/HomeController.java
+++ Gyakorlat/src/main/java/com/example/gyakorlat/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.gyakorlat;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private UserRepo userRepository;
 
     @GetMapping("/")
     public String index(HttpSession session, Model model) {
@@ -26,5 +29,50 @@ public class HomeController {
         model.addAttribute("user", null);
         model.addAttribute("page", "login");
         return "layout";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("pageTitle", "Regisztráció");
+        model.addAttribute("viewName", "register");
+        model.addAttribute("user", null);
+        model.addAttribute("page", "register");
+        return "layout";
+    }
+
+    @PostMapping("/register")
+    public String handleRegistration(
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam String name,
+            Model model) {
+        model.addAttribute("pageTitle", "Regisztráció");
+        model.addAttribute("viewName", "register");
+        model.addAttribute("user", null);
+        model.addAttribute("page", "register");
+
+        try {
+            if (userRepository.findByUsername(username).isPresent()) {
+                model.addAttribute("error", "A felhasználónév már foglalt!");
+                return "layout";
+            }
+
+            if (!username.isEmpty() && !password.isEmpty() && !name.isEmpty()) {
+                var passwordEncoder = new BCryptPasswordEncoder();
+
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(passwordEncoder.encode(password));
+                user.setName(name);
+                user.setRole("VISITOR");
+
+                userRepository.save(user);
+            }
+
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("error", "Hiba történt a regisztráció során!");
+            return "layout";
+        }
     }
 }
