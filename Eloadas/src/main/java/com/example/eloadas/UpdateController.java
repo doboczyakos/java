ad30f10386402b2af22383130178f2c0b885c74a
Dobóczy Ákos <akos.doboczy@tel.io>
2024-11-25 21:40:39 +0100

    Módosít almenü

--- Eloadas/src/main/java/com/example/eloadas/UpdateController.java
+++ Eloadas/src/main/java/com/example/eloadas/UpdateController.java
@@ -5,10 +5,7 @@ import com.example.eloadas.hulladekSzallitas.Szolgaltatas;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -16,8 +13,11 @@ import org.hibernate.cfg.Configuration;
 import java.time.ZoneId;
 import java.util.Date;
 
-public class CreateController {
+public class UpdateController {
     private final SessionFactory sessionFactory;
+
+    @FXML
+    private ComboBox<Lakig> recordComboBox;
 
     @FXML
     private ComboBox<Szolgaltatas> szolgaltatasComboBox;
@@ -28,15 +28,19 @@ public class CreateController {
     @FXML
     private TextField mennyisegField;
 
-    @FXML
-    private StackPane contentPane;
-
-    public CreateController() {
+    public UpdateController() {
         sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     }
 
     @FXML
     public void initialize() {
+        try (Session session = sessionFactory.openSession()) {
+            ObservableList<Lakig> records = FXCollections.observableArrayList(
+                    session.createQuery("from Lakig", Lakig.class).list()
+            );
+            recordComboBox.setItems(records);
+        }
+
         try (Session session = sessionFactory.openSession()) {
             ObservableList<Szolgaltatas> szolgaltatasok = FXCollections.observableArrayList(
                     session.createQuery("from Szolgaltatas", Szolgaltatas.class).list()
@@ -44,8 +48,38 @@ public class CreateController {
             szolgaltatasComboBox.setItems(szolgaltatasok);
         }
 
+        recordComboBox.valueProperty().addListener((obs, oldRecord, newRecord) -> {
+            if (newRecord != null) {
+                szolgaltatasComboBox.getSelectionModel().select(
+                        szolgaltatasComboBox.getItems().stream()
+                                .filter(s -> s.getId().equals(newRecord.getSzolgid()))
+                                .findFirst()
+                                .orElse(null)
+                );
+                igenyField.setValue(newRecord.getIgeny().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                mennyisegField.setText(String.valueOf(newRecord.getMennyiseg()));
+            }
+        });
+
+        recordComboBox.setCellFactory(param -> recordComboBoxSetup());
+        recordComboBox.setButtonCell(recordComboBoxSetup());
+
         szolgaltatasComboBox.setCellFactory(param -> szolgaltatasComboBoxSetup());
         szolgaltatasComboBox.setButtonCell(szolgaltatasComboBoxSetup());
+    }
+
+    private static ListCell<Lakig> recordComboBoxSetup() {
+        return new ListCell<>() {
+            @Override
+            protected void updateItem(Lakig item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getAzon().toString());
+                }
+            }
+        };
     }
 
     private static ListCell<Szolgaltatas> szolgaltatasComboBoxSetup() {
@@ -63,18 +97,19 @@ public class CreateController {
     }
 
     @FXML
-    private void addRecord() throws Exception {
+    private void updateRecord() {
+        Lakig selected = recordComboBox.getValue();
         Szolgaltatas selectedSzolgaltatas = szolgaltatasComboBox.getValue();
-        if (selectedSzolgaltatas != null) {
+
+        if (selected != null && selectedSzolgaltatas != null) {
             try (Session session = sessionFactory.openSession()) {
                 session.beginTransaction();
 
-                var newLakig = new Lakig();
-                newLakig.setSzolgid(selectedSzolgaltatas.getId());
-                newLakig.setIgeny(Date.from(igenyField.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-                newLakig.setMennyiseg(Integer.parseInt(mennyisegField.getText()));
+                selected.setSzolgid(selectedSzolgaltatas.getId());
+                selected.setIgeny(Date.from(igenyField.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                selected.setMennyiseg(Integer.parseInt(mennyisegField.getText()));
 
-                session.save(newLakig);
+                session.update(selected);
                 session.getTransaction().commit();
 
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
