b3349fbe9614c0df6f9929e51c03ca67f40c43a7
Dobóczy Ákos <akos.doboczy@tel.io>
2024-11-25 17:46:00 +0100

    JavaFX alkalmazás, Olvasás menü

---
+++ Eloadas/DB/szolgaltatas.sql
@@ -0,0 +1,5 @@
+INSERT INTO szolgaltatas (id, tipus, jelentes) VALUES (1, 'mua', 'Műanyag hulladék: PET palack, kozmetikai flakonok(PP+HDPE), szatyor, zacskó.');
+INSERT INTO szolgaltatas (id, tipus, jelentes) VALUES (2, 'uv', 'Üveg hulladék: színes és fehér üveg.');
+INSERT INTO szolgaltatas (id, tipus, jelentes) VALUES (3, 'zold', 'Zöldhulladék: komposztálható, kerti hulladékok.');
+INSERT INTO szolgaltatas (id, tipus, jelentes) VALUES (4, 'pa', 'Papírhulladékok: újságok, könyvek, kartondobozok.');
+INSERT INTO szolgaltatas (id, tipus, jelentes) VALUES (5, 'kom', 'Kommunális hulladék: szilárd, a lakókörnyezetünkben található, nem lebomló, nem veszélyes hulladék.');
---
+++ Eloadas/src/main/java/com/example/eloadas/MainController.java
@@ -0,0 +1,37 @@
+package com.example.eloadas;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+
+
+public class MainController {
+    @FXML
+    private StackPane contentPane;
+
+    @FXML
+    private void loadReadView() throws Exception {
+        loadView("read.fxml");
+    }
+
+    @FXML
+    private void loadCreateView() throws Exception {
+        loadView("create.fxml");
+    }
+
+    @FXML
+    private void loadUpdateView() throws Exception {
+        loadView("update.fxml");
+    }
+
+    @FXML
+    private void loadDeleteView() throws Exception {
+        loadView("delete.fxml");
+    }
+
+    private void loadView(String fxml) throws Exception {
+        Node view = FXMLLoader.load(getClass().getResource(fxml));
+        contentPane.getChildren().setAll(view);
+    }
+}
