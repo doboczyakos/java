9a449f9be5f126c5de1b71a66bf592e881217637
Onzelarg <101586565+Onzelarg@users.noreply.github.com>
2024-11-30 02:07:57 +0100

    MNB Soap
    MNB Soap implementáció, valahogy?

---
+++ Eloadas/src/main/java/com/example/eloadas/mnbSoap.java
@@ -0,0 +1,73 @@
+package com.example.eloadas;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import mnbSoap.MNBArfolyamServiceSoap;
+import mnbSoap.MNBArfolyamServiceSoapImpl;
+
+public class mnbSoap{
+
+    MNBArfolyamServiceSoapImpl impl;
+    MNBArfolyamServiceSoap service;
+    String filename;
+
+
+    mnbSoap() {
+        this.impl = new MNBArfolyamServiceSoapImpl();
+        this.service = impl.getCustomBindingMNBArfolyamServiceSoap();
+        this.filename="MNB.txt";
+    }
+
+    public String letoltes1(){
+        String adatok="";
+        try {
+            adatok=service.getCurrentExchangeRates();
+        }catch (Exception e){
+
+        }
+        writeToFile(adatok);
+        String result=adatok;
+        return result;
+
+    }
+
+    public String letoltes2(String currency,String startDate,String endDate){
+        String adatok="";
+        try {
+            adatok=service.getExchangeRates(startDate,endDate,currency);
+        }catch (Exception e){
+
+        }
+        String result=adatok;
+        writeToFile(adatok);
+        return result;
+    }
+
+    void clearFile(){
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write("");
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void writeToFile(String adat){
+        clearFile();
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write(adat);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+}
